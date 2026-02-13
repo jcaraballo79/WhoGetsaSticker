130ede4c233a34b641627e25127d22695c440576
Jorge J. Caraballo <jcaraballo79@gmail.com>
2026-02-13 17:15:56 -0500

    Initial Commit

---
+++ WhoGetsaSticker/WhoGetsaStickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoGetsaStickerApp.swift
+//  WhoGetsaSticker
+//
+//  Created by Jorge Caraballo on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct WhoGetsaStickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
